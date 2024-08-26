e4c16411459477db79a06889aec3d3b4cb587d6e
diaa562 <diaa13662@gmail.com>
2024-08-27 01:07:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// var diaa = document.getElementById('demo')
+// document.body.firstElementChild.style.color = 'red'
+// diaa.innerHTML = "ahmed"
+// var darky = document.getElementById('dark')
+// var lighty = document.getElementById('light')
+// function dark(){
+// document.body.style.backgroundColor = 'black'
+// document.body.style.color = 'white'
+
+
+// }
+// function light(){
+//     document.body.style.backgroundColor = 'white'
+//     document.body.style.color = 'black'
+
+
+// }
